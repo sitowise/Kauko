35d9b08da0747e4a5121df698dbd32febca77193
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2022-11-10 09:20:47 +0200

    Created triggers for versioning

---
+++ KaavaO.DbUp/Scripts/011_versioning_triggers.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION versioned_object_modified_trigger() RETURNS TRIGGER AS $$
+  BEGIN
+    NEW.latest_change := now();
+    IF TG_OP = 'INSERT' THEN
+      NEW.storage_time := now();
+    ELSIF TG_OP = 'UPDATE' THEN
+      NEW.storage_time = OLD.storage_time;
+    RETURN NEW;
+    END IF;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION create_local_id_trigger() RETURNS TRIGGER AS $$
+  BEGIN
+    IF NEW.local_id IS NULL THEN
+      NEW.local_id := NEW.identity_id || '.' || uuid_generate_v4();
+    END IF;
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
