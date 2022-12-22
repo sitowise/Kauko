ac747a685964c3eb43983d9fc30f49c4d8602b42
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2022-12-22 13:54:46 +0200

    Refactored refresh_validity() function

--- kaavao/database/project_updater/scripts/016_fix_for_validity_triggers.sql
+++ kaavao/database/project_updater/scripts/016_fix_for_validity_triggers.sql
@@ -314,10 +314,8 @@ GRANT EXECUTE ON FUNCTION SCHEMANAME.inherit_validity() TO qgis_admin;
 -- CREATE update_validity TRIGGER
 
 CREATE FUNCTION "SCHEMANAME".update_validity()
-    RETURNS trigger
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE NOT LEAKPROOF
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
 AS $BODY$
 BEGIN
     PERFORM SCHEMANAME.refresh_validity();
@@ -644,189 +642,159 @@ execute procedure SCHEMANAME.update_validity();
 
 CREATE OR REPLACE FUNCTION "SCHEMANAME".refresh_validity()
     RETURNS void
-    LANGUAGE 'sql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
+    LANGUAGE plpgsql
 AS $BODY$
-UPDATE SCHEMANAME.spatial_plan sp
-SET validity = 1
-WHERE sp.validity = 4
-  AND sp.valid_from <= Current_Date
-  AND (sp.valid_to IS NULL OR sp.valid_to >= Current_Date);
+BEGIN
+  UPDATE SCHEMANAME.spatial_plan sp
+  SET validity = 1
+  WHERE sp.validity = 4
+    AND sp.validity_time @> Current_Date;
 
-UPDATE SCHEMANAME.spatial_plan sp
-SET validity = 3
-WHERE sp.validity <> 3
-  AND sp.valid_to < Current_Date;
+  UPDATE SCHEMANAME.spatial_plan sp
+  SET validity = 3
+  WHERE sp.validity <> 3
+    AND upper(sp.validity_time) < Current_Date;
 
-UPDATE SCHEMANAME.zoning_element ze
-SET validity = 1
-WHERE ze.validity = 4
-  AND ze.valid_from <= Current_Date
-  AND (ze.valid_to IS NULL OR ze.valid_to >= Current_Date);
+  UPDATE SCHEMANAME.zoning_element ze
+  SET validity = 1
+  WHERE ze.validity = 4
+    AND ze.validity_time @> Current_Date;
 
-UPDATE SCHEMANAME.zoning_element ze
-SET validity   = 1,
-    valid_from = sp.valid_from,
-    valid_to   = sp.valid_to
-FROM SCHEMANAME.spatial_plan sp
-WHERE sp.planning_object_identifier = ze.fk_spatial_plan
-  AND ze.validity = 4
-  AND (ze.valid_from IS NULL OR ze.valid_from <= sp.valid_from)
-  AND (ze.valid_to IS NULL OR ze.valid_to >= sp.valid_to)
-  AND sp.validity = 1;
+  UPDATE SCHEMANAME.zoning_element ze
+  SET
+    validity = 1,
+    valid_from = GREATEST(ze.valid_from, sp.valid_from),
+    valid_to = LEAST(ze.valid_to, sp.valid_to)
+  FROM SCHEMANAME.spatial_plan sp
+  WHERE sp.local_id = ze.spatial_plan
+    AND ze.validity = 4
+    AND sp.validity = 1;
 
-UPDATE SCHEMANAME.zoning_element ze
-SET validity = 3
-WHERE ze.validity <> 3
-  AND ze.valid_to < Current_Date;
+  UPDATE SCHEMANAME.zoning_element ze
+  SET validity = 3
+  WHERE ze.validity <> 3
+    AND upper(ze.validity_time) < Current_Date;
 
-UPDATE SCHEMANAME.zoning_element ze
-SET validity = 3,
-    valid_to = sp.valid_to
-FROM SCHEMANAME.spatial_plan sp
-WHERE sp.planning_object_identifier = ze.fk_spatial_plan
-  AND ze.validity <> 3
-  AND sp.validity = 3;
+  UPDATE SCHEMANAME.zoning_element ze
+  SET validity = 3,
+      valid_to = sp.valid_to
+  FROM SCHEMANAME.spatial_plan sp
+  WHERE sp.local_id = ze.spatial_plan
+    AND ze.validity <> 3
+    AND sp.validity = 3;
 
-UPDATE SCHEMANAME.planned_space ps
-SET validity = 1
-WHERE ps.validity = 4
-  AND ps.valid_from <= Current_Date
-  AND (ps.valid_to IS NULL OR ps.valid_to >= Current_Date);
+  UPDATE SCHEMANAME.planned_space ps
+  SET validity = 1
+  WHERE ps.validity = 4
+    AND ps.validity_time @> Current_Date;
 
-UPDATE SCHEMANAME.planned_space ps
-SET validity   = 1,
-    valid_from = ze.valid_from,
-    valid_to   = ze.valid_to
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_planned_space ze_ps
-WHERE ps.planning_object_identifier = ze_ps.planned_space_id
-  AND ze_ps.zoning_element_id = ze.planning_object_identifier
-  AND ps.validity = 4
-  AND ze.validity = 1
-  AND ps.valid_from IS NULL
-  AND ps.valid_to IS NULL;
+  UPDATE SCHEMANAME.planned_space ps
+  SET
+    validity = 1,
+    valid_from = GREATEST(ps.valid_from, ze.valid_from),
+    valid_to = LEAST(ps.valid_from, ze.valid_from)
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_planned_space ze_ps
+    ON ze_ps.zoning_element_local_id = ze.local_id
+    AND ze.validity = 1
+  WHERE ps.local_id = ze_ps.planned_space_local_id
+    AND ps.validity = 4;
 
-UPDATE SCHEMANAME.planned_space ps
-SET validity = 3
-WHERE ps.validity <> 3
-  AND ps.valid_to < Current_Date;
+  UPDATE SCHEMANAME.planned_space ps
+  SET validity = 3
+  WHERE ps.validity <> 3
+    AND upper(ps.validity_time) < Current_Date;
 
-UPDATE SCHEMANAME.planned_space ps
-SET validity = 3,
-    valid_to = ze.valid_to
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_planned_space ze_ps
-WHERE ps.planning_object_identifier = ze_ps.planned_space_id
-  AND ze_ps.zoning_element_id = ze.planning_object_identifier
-  AND NOT Exists(SELECT *
-                 FROM SCHEMANAME.zoning_element ze
-                          INNER JOIN
-                      SCHEMANAME.zoning_element_planned_space ze_ps
-                      ON ze_ps.zoning_element_id = ze.planning_object_identifier
-                 WHERE ze.validity <> 3
-                   AND ze_ps.planned_space_id = ps.planning_object_identifier)
-  AND ps.validity <> 3;
+  UPDATE SCHEMANAME.planned_space ps
+  SET validity = 3,
+      valid_to = ze.valid_to
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_planned_space ze_ps
+    ON ze_ps.zoning_element_local_id = ze.local_id
+    AND ze.validity = 3
+  WHERE ps.local_id = ze_ps.planned_space_local_id
+    AND ps.validity <> 3;
 
-UPDATE SCHEMANAME.planning_detail_line pdl
-SET validity = 1
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_plan_detail_line ze_pdl
-WHERE ze_pdl.zoning_id = ze.planning_object_identifier
-  AND pdl.planning_object_identifier = ze_pdl.plan_detail_line_id
-  AND pdl.validity = 4
-  AND ze.validity = 1;
+  UPDATE SCHEMANAME.planning_detail_line pdl
+  SET validity = 1
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_plan_detail_line ze_pdl
+    ON ze_pdl.zoning_element_local_id = ze.local_id
+    AND ze.validity = 1
+  WHERE pdl.local_id = ze_pdl.planning_detail_line_local_id
+    AND pdl.validity = 4;
 
-UPDATE SCHEMANAME.planning_detail_line pdl
-SET validity = 3
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_plan_detail_line ze_pdl
-WHERE pdl.planning_object_identifier = ze_pdl.plan_detail_line_id
-  AND ze_pdl.zoning_id = ze.planning_object_identifier
-  AND pdl.validity <> 3
-  AND ze.validity = 3;
+  UPDATE SCHEMANAME.planning_detail_line pdl
+  SET validity = 3
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_plan_detail_line ze_pdl
+    ON ze_pdl.zoning_element_local_id = ze.local_id
+    AND ze.validity = 3
+  WHERE pdl.local_id = ze_pdl.planning_detail_line_local_id
+    AND pdl.validity <> 3;
 
-UPDATE SCHEMANAME.planning_detail_point pdp
-SET validity = 1
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_plan_detail_point ze_pdp
-WHERE pdp.planning_object_identifier = ze_pdp.plan_detail_point_id
-  AND ze_pdp.zoning_id = ze.planning_object_identifier
-  AND pdp.validity = 4
-  AND ze.validity = 1;
+  UPDATE SCHEMANAME.describing_line dl
+  SET validity = 1
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_describing_line ze_dl
+    ON ze_dl.zoning_element_local_id = ze.local_id
+    AND ze.validity = 1
+  WHERE dl.identifier = ze_dl.describing_line_id
+    AND dl.validity = 4;
 
-UPDATE SCHEMANAME.planning_detail_point pdp
-SET validity = 3
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_plan_detail_point ze_pdp
-WHERE pdp.planning_object_identifier = ze_pdp.plan_detail_point_id
-  AND ze_pdp.zoning_id = ze.planning_object_identifier
-  AND pdp.validity <> 3
-  AND ze.validity = 3;
+  UPDATE SCHEMANAME.describing_line dl
+  SET validity = 3
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_describing_line ze_dl
+    ON ze_dl.zoning_element_local_id = ze.local_id
+    AND ze.validity = 3
+  WHERE dl.identifier = ze_dl.describing_line_id
+    AND dl.validity <> 3;
 
-UPDATE SCHEMANAME.describing_line dl
-SET validity = 1
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_describing_line ze_dl
-WHERE dl.identifier = ze_dl.describing_line_id
-  AND ze_dl.zoning_id = ze.planning_object_identifier
-  AND dl.validity = 4
-  AND ze.validity = 1;
+  UPDATE SCHEMANAME.describing_text dt
+  SET validity = 1
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_describing_text ze_dt
+    ON ze_dt.zoning_element_local_id = ze.local_id
+    AND ze.validity = 1
+  WHERE dt.identifier = ze_dt.describing_text_id
+    AND dt.validity = 4;
 
-UPDATE SCHEMANAME.describing_line dl
-SET validity = 3
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_describing_line ze_dl
-WHERE dl.identifier = ze_dl.describing_line_id
-  AND ze_dl.zoning_id = ze.planning_object_identifier
-  AND dl.validity <> 3
-  AND ze.validity = 3;
+  UPDATE SCHEMANAME.describing_text dt
+  SET validity = 3
+  FROM SCHEMANAME.zoning_element ze
+  JOIN SCHEMANAME.zoning_element_describing_text ze_dt
+    ON ze_dt.zoning_element_local_id = ze.local_id
+    AND ze.validity = 3
+  WHERE dt.identifier = ze_dt.describing_text_id
+    AND dt.validity <> 3;
 
-UPDATE SCHEMANAME.describing_text dt
-SET validity = 1
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_describing_text ze_dt
-WHERE dt.identifier = ze_dt.describing_text_id
-  AND ze_dt.zoning_id = ze.planning_object_identifier
-  AND dt.validity = 4
-  AND ze.validity = 1;
+  WITH RECURSIVE valid_spatial_plans(local_id) AS (
+      SELECT DISTINCT sp.local_id
+      FROM SCHEMANAME.spatial_plan sp
+        INNER JOIN SCHEMANAME.zoning_element ze
+          ON ze.spatial_plan = sp.local_id
+      WHERE ze.validity = 1
+        AND sp.validity <> 1
+      EXCEPT
+      SELECT sp2.local_id
+      FROM SCHEMANAME.spatial_plan sp2
+        INNER JOIN SCHEMANAME.zoning_element ze2
+          ON ze2.spatial_plan = sp2.local_id
+      WHERE ze2.validity <> 1
+  )
+  UPDATE SCHEMANAME.spatial_plan sp
+  SET validity = 1
+  FROM valid_spatial_plans vsp
+  WHERE sp.local_id = vsp.local_id;
 
-UPDATE SCHEMANAME.describing_text dt
-SET validity = 3
-FROM SCHEMANAME.zoning_element ze,
-     SCHEMANAME.zoning_element_describing_text ze_dt
-WHERE dt.identifier = ze_dt.describing_text_id
-  AND ze_dt.zoning_id = ze.planning_object_identifier
-  AND dt.validity <> 3
-  AND ze.validity = 3;
-
-WITH RECURSIVE valid_spatial_plans(planning_object_identifier) AS (
-    SELECT sp.planning_object_identifier
-    FROM SCHEMANAME.spatial_plan sp
-             INNER JOIN
-         SCHEMANAME.zoning_element ze ON ze.fk_spatial_plan = sp.planning_object_identifier
-    WHERE ze.validity = 1
-      AND sp.validity <> 1
-        EXCEPT
-    SELECT SCHEMANAME.spatial_plan.planning_object_identifier
-    FROM SCHEMANAME.spatial_plan
-             INNER JOIN
-         SCHEMANAME.zoning_element ON SCHEMANAME.zoning_element.fk_spatial_plan =
-                                     SCHEMANAME.spatial_plan.planning_object_identifier
-    WHERE SCHEMANAME.zoning_element.validity <> 1
-)
-UPDATE SCHEMANAME.spatial_plan sp
-SET validity = 1
-FROM valid_spatial_plans
-WHERE sp.planning_object_identifier = valid_spatial_plans.planning_object_identifier;
-
-UPDATE SCHEMANAME.spatial_plan sp
-SET validity = 2
-FROM SCHEMANAME.zoning_element ze
-WHERE sp.planning_object_identifier = ze.fk_spatial_plan
-  AND ze.validity <> 1
-  AND sp.validity = 1;
+  UPDATE SCHEMANAME.spatial_plan sp
+  SET validity = 2
+  FROM SCHEMANAME.zoning_element ze
+  WHERE sp.local_id = ze.spatial_plan
+    AND ze.validity <> 1
+    AND sp.validity = 1;
+END;
 $BODY$;
 
 
