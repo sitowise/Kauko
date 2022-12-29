0c2bbbc58f13713032ba40840a067dfbc67b9aa3
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2022-12-29 13:26:48 +0200

    Updated schema version table to support master plans

---
+++ Kauko.DbUp/Scripts/012_update_schema_information_table.sql
@@ -0,0 +1,16 @@
+﻿ALTER TABLE public.schema_information
+  DROP CONSTRAINT check_combination;
+
+ALTER TABLE public.schema_information
+  RENAME column combination TO is_master_plan;
+
+ALTER TABLE public.schema_information
+  ADD CONSTRAINT check_is_master_plan CHECK (
+    CASE
+      WHEN name like '%y' AND is_master_plan IS TRUE
+        THEN TRUE
+      WHEN name not like '%y' AND is_master_plan IS FALSE
+        THEN TRUE
+      ELSE FALSE
+    END
+  );
