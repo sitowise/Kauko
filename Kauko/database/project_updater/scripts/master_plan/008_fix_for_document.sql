445ce38ed640d69e62c5c18e0bf932556e0c8109
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2023-02-01 15:23:23 +0200

    Added type to document

--- Kauko/database/project_updater/scripts/master_plan/008_fix_for_document.sql
+++ Kauko/database/project_updater/scripts/master_plan/008_fix_for_document.sql
@@ -1,3 +1,16 @@
 ALTER TABLE SCHEMANAME."document"
   ADD COLUMN additional_information_link VARCHAR,
-  ADD COLUMN metadata VARCHAR;
+  ADD COLUMN metadata VARCHAR,
+  ADD COLUMN "type" VARCHAR;
+
+UPDATE SCHEMANAME."document" SET "type" = '99';
+
+ALTER TABLE SCHEMANAME."document"
+  ALTER COLUMN "type" SET NOT NULL;
+
+ALTER TABLE SCHEMANAME."document"
+  ADD CONSTRAINT fk_document_type
+  FOREIGN KEY ("type")
+  REFERENCES code_lists.document_kind ("type")
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE;
