005c4287a0802e4ccf58a73fe5dd09f3178997d2
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2023-04-06 08:42:30 +0300

    Fixed typo

--- Kauko/database/project_updater/scripts/detailed_plan/026_update_finnish_muncipality_code_list_reference.sql
+++ Kauko/database/project_updater/scripts/detailed_plan/026_update_finnish_muncipality_code_list_reference.sql
@@ -2,4 +2,4 @@ ALTER TABLE SCHEMANAME.spatial_plan
     ADD CONSTRAINT fk_finnish_muncipality FOREIGN KEY (land_administration_authority)
     REFERENCES code_lists.finnish_municipality (codevalue)
     ON UPDATE CASCADE
-    ON DELETER RESTRICT;
+    ON DELETE RESTRICT;
