c2ab2ad99a36c6ab5617bd6f936e82642a0a1d2c
Valtteri Lappalainen <valtteri.lappalainen@sitowise.com>
2023-04-13 08:08:50 +0300

    Fixes to masterplan scripts

--- Kauko/database/project_updater/scripts/master_plan/017_fix_zoning_element_name_column.sql
+++ Kauko/database/project_updater/scripts/master_plan/017_fix_zoning_element_name_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE SCHEMANAME.zoning_element
     ALTER COLUMN "name" TYPE jsonb
-    USING json_build_object('fi', "name"),
+    USING CASE WHEN "name" IS NULL THEN NULL ELSE json_build_object('fin', "name") END,
 	ADD CONSTRAINT check_language_string CHECK (check_language_string(name));
