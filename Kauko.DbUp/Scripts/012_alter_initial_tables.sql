974afb7f284f1a48227f1f7136f5afc075fb6299
Tuomas Kujala <tuomas.kujala@sitowise.com>
2024-12-31 14:58:53 +0200

    fix: add foreign key for spatial_plan --> decision_maker

--- Kauko.DbUp/Scripts/012_alter_initial_tables.sql
+++ Kauko.DbUp/Scripts/012_alter_initial_tables.sql
@@ -373,4 +373,11 @@ ADD COLUMN letter_identifier VARCHAR(255);
 ALTER TABLE $SCHEMANAME$.plan_regulation_group
 ADD COLUMN color_number VARCHAR(255);
 
+-- Table: $SCHEMANAME$.spatial_plan
 
+ALTER TABLE $SCHEMANAME$.spatial_plan
+ADD COLUMN fk_decision_maker TEXT,
+ADD CONSTRAINT decision_maker_local_id_fkey FOREIGN KEY (fk_decision_maker)
+        REFERENCES $SCHEMANAME$.plan_decision (local_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT;
