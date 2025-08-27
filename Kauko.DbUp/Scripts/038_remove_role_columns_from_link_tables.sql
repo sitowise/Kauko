22fbdc72f5728474a373983f371d68905766817a
Walter Kronqvist <walter.kronqvist@sitowise.com>
2025-08-27 10:29:45 +0300

    feat: remove role columns from link tables, make group_number nullable

---
+++ Kauko.DbUp/Scripts/038_remove_role_columns_from_link_tables.sql
@@ -0,0 +1,5 @@
+ALTER TABLE raasepori_gk24.decision_document DROP COLUMN IF EXISTS "role";
+ALTER TABLE raasepori_gk24.document_document DROP COLUMN IF EXISTS "role";
+ALTER TABLE raasepori_gk24.plan_guidance_document DROP COLUMN IF EXISTS "role";
+ALTER TABLE raasepori_gk24.plan_regulation_document DROP COLUMN IF EXISTS "role";
+ALTER TABLE raasepori_gk24.spatial_plan_document DROP COLUMN IF EXISTS "role";
