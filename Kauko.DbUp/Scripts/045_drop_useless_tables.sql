a286469f386db9b2bd9d3862ccc1bb57b7ee7a92
Tuomas Kujala <tuomas.kujala@sitowise.com>
2026-01-14 11:36:04 +0200

    feat: drop tables not in use

---
+++ Kauko.DbUp/Scripts/045_drop_useless_tables.sql
@@ -0,0 +1,7 @@
+-- Drop tables not in use
+---------------------------
+
+DROP TABLE IF EXISTS $SCHEMANAME$.positive_decimal_range;
+DROP TABLE IF EXISTS $SCHEMANAME$.positive_decimal_value;
+DROP TABLE IF EXISTS $SCHEMANAME$.positive_numeric_range;
+DROP TABLE IF EXISTS $SCHEMANAME$.positive_numeric_value;
